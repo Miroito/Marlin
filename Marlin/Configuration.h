d3b232486a3d0dbc82029c1d356b4ca661fcbe27
Miroito <alban.vachette@gmail.com>
2021-09-18 23:43:12 +0200

    Edit motherboard and set screen

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -139,7 +139,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_RAMPS_14_EFB
+  #define MOTHERBOARD BOARD_GT2560_REV_A_PLUS
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
@@ -2194,7 +2194,7 @@
 // Requires Testato, Koepel softwarewire library and
 // Andriy Golovnya's LiquidCrystal_AIP31068 library.
 //
-//#define YHCB2004
+#define YHCB2004
 
 //
 // Original RADDS LCD Display+Encoder+SDCardReader
