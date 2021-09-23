7a2e661447f564dc5e9f82de3e5c4e45ed0545ca
Miroito <alban.vachette@gmail.com>
2021-09-23 14:04:16 +0200

    Height setup

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -779,7 +779,7 @@
   #define DELTA_DIAGONAL_ROD 196.0        // (mm)
 
   // Distance between bed and nozzle Z home position
-  #define DELTA_HEIGHT 240                // (mm) Get this value from G33 auto calibrate
+  #define DELTA_HEIGHT 228                // (mm) Get this value from G33 auto calibrate
 
   #define DELTA_ENDSTOP_ADJ { 0.0, 0.0, 0.0 } // Get these values from G33 auto calibrate
 
